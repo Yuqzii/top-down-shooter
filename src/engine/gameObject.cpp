cdd5c42d97bb0dd672baa9974ff2fca5fb18c114
Yuqzii <78998714+Yuqzii@users.noreply.github.com>
2025-04-15 13:53:29 +0000

    chore: fix linting issues

--- src/engine/gameObject.cpp
+++ src/engine/gameObject.cpp
@@ -14,11 +14,19 @@
 // Create global macro or sum for size instead of 3?
 GameObject::GameObject()
 	: deleteObject{false},
-	  srcRect{0, 0, 32, 32}, destRect{0, 0, srcRect.w * 3, srcRect.h * 3},
-	  pivotOffset{0, 0}, isAnimated{false}, animationCounter{0}, animationSequence{0}, prevFrame{0},
-	  boundingCircle{500.0f}, useCollision{false}, collisionList{}, collisionType{},
-	  rotation{0}, flipType{SDL_FLIP_NONE}
-{}
+	  srcRect{0, 0, 32, 32},
+	  destRect{0, 0, srcRect.w * 3, srcRect.h * 3},
+	  pivotOffset{0, 0},
+	  isAnimated{false},
+	  animationCounter{0},
+	  animationSequence{0},
+	  prevFrame{0},
+	  boundingCircle{500.0f},
+	  useCollision{false},
+	  collisionList{},
+	  collisionType{},
+	  rotation{0},
+	  flipType{SDL_FLIP_NONE} {}
 
 void GameObject::initialize(const vector2Df& startPosition, const Scene& scene) {
 	// Load texture
