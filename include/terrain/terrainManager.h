ff0e2290e799b1c97e1b157c9de13c2e7e51a568
yuqzii <p4r4yuqzii@gmail.com>
2025-04-16 23:24:27 +0200

    refactor(terrain): separate function for updating
    Create separate function for updating the terrain, so that it is not
    recalculated every frame.

--- include/terrain/terrainManager.h
+++ include/terrain/terrainManager.h
@@ -1,7 +1,9 @@
 #pragma once
 
+#include <optional>
 #include <vector>
 #include "SDL2/SDL_pixels.h"
+#include "SDL2/SDL_rect.h"
 
 class SDL_Renderer;
 
@@ -9,11 +11,16 @@ class TerrainManager {
 public:
 	TerrainManager(const std::vector<std::vector<char>>& terrainMap, const SDL_Color& color);
 
+
+	void update();
 	void render(SDL_Renderer* renderer) const;
 
 private:
 	std::vector<std::vector<char>> terrainMap;
 	const size_t xSize, ySize;
+	constexpr static const int pixelSizeMultiplier = 5;
+
+	std::vector<std::vector<SDL_Rect>> renderRects;
 
 	SDL_Color color;
 };
--- src/terrain/terrainManager.cpp
+++ src/terrain/terrainManager.cpp
@@ -5,24 +5,39 @@
 TerrainManager::TerrainManager(const std::vector<std::vector<char>>& terrainMap_,
 							   const SDL_Color& color_)
 	: terrainMap{terrainMap_}, xSize{terrainMap.size()}, ySize{terrainMap[0].size()},
-	  color(color_)
-{}
+	  color(color_) {
+	renderRects.resize(xSize);
+	for (auto& rectList : renderRects)
+		rectList.resize(ySize);
+
+	update();
+}
+
+void TerrainManager::update() {
+	for (int x = 0; x < xSize; x++) {
+		for (int y = 0; y < ySize; y++) {
+			// Add render rect if there is terrain at the current position
+			if (terrainMap[x][y]) {
+				// Does this pixel need recalculation?
+				if (renderRects[x][y].w != 0) continue;
+
+				renderRects[x][y].x = x * Game::pixelSize * pixelSizeMultiplier;
+				renderRects[x][y].y = y * Game::pixelSize * pixelSizeMultiplier;
+				renderRects[x][y].w = renderRects[x][y].h = Game::pixelSize * pixelSizeMultiplier;
+			}
+			else {
+				// Zero rect so that is is not rendered
+				renderRects[x][y].w = renderRects[x][y].h = 0;
+			}
+		}
+	}
+}
 
 void TerrainManager::render(SDL_Renderer* renderer) const {
 	SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
 
-	for (int x = 0; x < xSize; x++) {
-		std::vector<SDL_Rect> rects;
-		rects.reserve(ySize);
-		for (int y = 0; y < ySize; y++) {
-			// Add render rect if there is terrain at the current position
-			if (terrainMap[x][y]) {
-				rects.push_back(SDL_Rect{x * Game::pixelSize * 5, y * Game::pixelSize * 5,
-					Game::pixelSize * 5, Game::pixelSize * 5});
-			}
-		}
-
-		// Draw all the terrain "pixels" for the current x-position
-		SDL_RenderFillRects(renderer, &rects[0], rects.size());
+	for (auto& rectList : renderRects) {
+		SDL_RenderFillRects(renderer, &rectList[0], rectList.size());
 	}
+	// Draw all the terrain "pixels" for the current x-position
 }
