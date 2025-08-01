9a7498a0a7452d426c9c3b72e632419731463b8b
yuqzii <p4r4yuqzii@gmail.com>
2025-08-01 18:59:52 +0200

    fix: terrain printing sideways

--- src/terrain/terrain.cpp
+++ src/terrain/terrain.cpp
@@ -6,12 +6,11 @@ Terrain::Terrain(const size_t xSize, const size_t ySize) {
 	map.resize(ySize, std::vector<unsigned char>(xSize));
 }
 
-Terrain::Terrain(const std::vector<std::vector<unsigned char>>& map)
-	: map{map} {}
+Terrain::Terrain(const std::vector<std::vector<unsigned char>>& map) : map{map} {}
 
 void Terrain::printTerrain() const {
-	for (size_t x = 0; x < getXSize(); x++) {
-		for (size_t y = 0; y < getYSize(); y++) std::cout << (map[y][x] ? '#' : '.');
+	for (size_t y = 0; y < getYSize(); y++) {
+		for (size_t x = 0; x < getXSize(); x++) std::cout << (map[y][x] ? '#' : '.');
 		std::cout << '\n';
 	}
 	std::cout << '\n';
