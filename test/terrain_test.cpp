f839d1a4aaf6506aa49f9cffb16d9b8626d9322c
yuqzii <p4r4yuqzii@gmail.com>
2025-08-05 14:10:48 +0200

    fix: terrain tests

--- test/terrain_test.cpp
+++ test/terrain_test.cpp
@@ -4,33 +4,34 @@
 #include "terrain/terrainManager.h"
 
 TEST(Terrain, CollisionGeneration) {
-	const std::vector<std::vector<char>> terrainMap{
-		std::vector<char>{1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0},
-		std::vector<char>{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0},
-		std::vector<char>{1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0},
-		std::vector<char>{1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0},
-		std::vector<char>{1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0},
-		std::vector<char>{0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0},
-		std::vector<char>{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-		std::vector<char>{0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0},
-		std::vector<char>{0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0},
-		std::vector<char>{0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0},
-		std::vector<char>{0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0},
-		std::vector<char>{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-		std::vector<char>{0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0},
-		std::vector<char>{0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0},
-		std::vector<char>{1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
-		std::vector<char>{0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0},
-		std::vector<char>{0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0},
-		std::vector<char>{0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0},
-		std::vector<char>{0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0},
-		std::vector<char>{0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0},
+	const std::vector<std::vector<unsigned char>> terrainMap{
+		std::vector<unsigned char>{1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		std::vector<unsigned char>{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		std::vector<unsigned char>{1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		std::vector<unsigned char>{1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		std::vector<unsigned char>{1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		std::vector<unsigned char>{0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		std::vector<unsigned char>{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		std::vector<unsigned char>{0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		std::vector<unsigned char>{0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		std::vector<unsigned char>{0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		std::vector<unsigned char>{0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		std::vector<unsigned char>{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		std::vector<unsigned char>{0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		std::vector<unsigned char>{0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		std::vector<unsigned char>{1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		std::vector<unsigned char>{0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		std::vector<unsigned char>{0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		std::vector<unsigned char>{0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		std::vector<unsigned char>{0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		std::vector<unsigned char>{0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 	};
 
+	Terrain terrain{std::move(terrainMap)};
 	Game game{"", 1, 1};
 	MockScene scene{game};
-	TerrainManager manager{terrainMap, SDL_Color{1, 1, 1, 1}, scene};
-	manager.updateCollisions();
+	const std::size_t chunkSize = terrainMap.size();
+	TerrainManager manager{terrain, chunkSize, 1, SDL_Color{1, 1, 1, 1}, scene};
 
 	constexpr int expected = 44;
 	EXPECT_TRUE(scene.objCount() == expected)
